1ab46f86a6c2efab3a19d41c660e74b54cf54e5d
Arthur Iques <arthuriquesconta1@gmail.com>
2024-11-18 12:57:05 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import conectaDataBase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaDataBase();
+conexao.on("error", (erro) => {
+  console.error("error de conexão:", erro);
+});
+
+conexao.once("open", () => {
+  console.log("conexão feita com sucesso!!");
+});
+
+const app = express();
+routes(app);
+
+export default app;
